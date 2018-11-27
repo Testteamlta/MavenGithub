7138622fb30e5c86a50bc9c3e1e698a8f77e4b0b
prakash.ramasamy <prakash.ramasamy@L03969.lta.org.uk>
2018-11-27 15:44:07 +0000

    test

---
+++ ThirdProject/src/test/java/com/org/Maventhree/ThirdProject/ToTry.java
@@ -0,0 +1,13 @@
+package com.org.Maventhree.ThirdProject;
+
+import org.testng.annotations.Test;
+
+public class ToTry {
+
+	
+	@Test
+	public void Login()
+	{
+		System.out.println("LOGGING IN TO THE ACCOUNT");
+	}
+}
