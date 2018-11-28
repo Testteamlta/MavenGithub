14b6c9ce81d7e9fd6168e7fd22c4c9e906f39173
prakash.ramasamy <prakash.ramasamy@L03969.lta.org.uk>
2018-11-28 18:26:38 +0000

    Testing1

---
+++ ProjectFive/src/test/java/FifthProject/ProjectFive/Testing.java
@@ -0,0 +1,14 @@
+package FifthProject.ProjectFive;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+
+	
+	@Test
+	public void Login()
+	{
+		System.out.println("LOGGING IN TO THE ACCOUNT");
+	}
+	
+}
